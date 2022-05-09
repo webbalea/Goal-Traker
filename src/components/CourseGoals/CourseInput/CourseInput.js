0c035f1561fc25becd277dbcec21f2c5737330c1
Raul Balea <rb@insomniacdesign.com>
2022-05-09 09:14:00 +0300

    first commit

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    setIsValid(true);
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      console.log('Please enter a value');
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
